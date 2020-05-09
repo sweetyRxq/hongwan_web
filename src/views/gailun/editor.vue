87de9522a58872d28a15b73b57c6dcf5889c413c
root <root@f0ec77779a87>
2020-05-09 08:04:02 +0000

    push at datetime

---
+++ src/views/gailun/editor.vue
@@ -0,0 +1,67 @@
+//GENERATE_START
+<template>
+	<el-dialog :title="title" :visible.sync="visible">
+		<el-form v-if="visible == true" ref="saveForm" :model="entity" :rules="rules" status-icon label-width="120px">
+			<el-form-item :label="$t('gailun.qmdj')">
+			   <el-input v-if="!''" v-model.trim="entity.qmdj"></el-input>
+			</el-form-item>
+			<el-form-item :label="$t('gailun.abab')">
+			   <el-input v-if="!''" v-model.trim="entity.abab"></el-input>
+			</el-form-item>
+			<el-form-item :label="$t('gailun.aabb')">
+			   <el-input v-if="!''" v-model.trim="entity.aabb"></el-input>
+			</el-form-item>
+		</el-form>
+	    <span slot="footer" class="dialog-footer">
+	      	<el-button @click="visible = false">{{$t('actions.cancel')}}</el-button>
+	      	<el-button type="primary" @click="onOK">{{$t('actions.ok')}}</el-button>
+	    </span>
+	</el-dialog>
+</template>
+<script>
+	import { getJson }  from '@/utils/restapi'
+	import { numberInput,numberSpotInput,notChinese} from '@/utils/validate'
+	export default {
+		name: 'ClsTestEditor',
+		data() {
+			return {
+				title : 'Untitled',
+				visible: false,
+				entity: {},
+				mode : null,
+				chaincodeOptions: [],
+				rules: {		 
+                }
+			}
+		},
+		mounted(){
+			// Do nothing
+		},
+		methods:{
+			open(entity){
+				this.visible = true
+				this.entity = entity
+				if(this.entity.null){
+					this.mode = 'EDIT'
+					this.title = this.$t('gailun.edit')
+				}else{
+					this.mode = 'NEW'
+					this.title = this.$t('gailun.addnew')
+				}
+			},
+			getMode(){
+				return this.mode
+			},
+			getEntity(){
+				return this.entity
+			},
+			close(){
+				this.visible = false
+			},
+			onOK(){
+				this.$emit("onOK", this.entity)
+			}
+		}
+	}
+</script>
+//GENERATE_END
